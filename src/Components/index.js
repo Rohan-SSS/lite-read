c550b8d7923861fae072d9a118d1fe3494713da2
RohanSSS <singhrohan2533@gmail.com>
2023-10-29 20:17:16 +0530

    minor fixes

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,5 @@
 export { default as BotComp } from "./BotComp/BotComp";
 export { default as MediaCard } from './MediaCard/MediaCard';
 export { default as Navbar } from './Navbar/Navbar';
+export { default as TextComp } from './TextComp/TextComp';
 
