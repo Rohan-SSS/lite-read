f81ed20808d1125a4204f9417794ac9c558f0edc
RohanSSS <singhrohan2533@gmail.com>
2023-10-29 19:46:25 +0530

    added textcon

--- src/Containers/TextCon/TextCon.jsx
+++ src/Containers/TextCon/TextCon.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { TextComp } from "../../Components";
 import "./TextCon.css";
 
 const TextCon = () => {
-  return <div className="textcon">TextConTextConTextConText</div>;
+  return (
+    <div className="textcon">
+      <TextComp />
+    </div>
+  );
 };
 
 export default TextCon;
